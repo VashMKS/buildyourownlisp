2a9a2c3522e44282a367e475ce6ae1ec055ae062
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2021-06-30 22:05:59 +0200

    feat: Add line edition and history to prompt

--- prompt.c
+++ prompt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare a buffer for user input
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
 	
@@ -11,9 +12,10 @@ int main(int argc, char** argv) {
 	
 	// Endlessly prompt for input and reply back
 	while (1) {
-		fputs("lsp>", stdout);
-		fgets(input, 2048, stdin);
-		printf("No you're a %s", input);
+		char* input = readline("lsp> ");
+		add_history(input);
+		printf("No you're a %s\n", input);
+		free(input);
 	}
 	
 	return 0;

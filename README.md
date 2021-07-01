3e4b36bab33423714c9bcef5789cae47fc419281
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2021-07-01 07:39:48 +0200

    feat: Implement Polish notation parser (chapter 6) add mpc dependencies move snippets to README.md add more detailed comments and remove separate notes

--- README.md
+++ README.md
@@ -3,3 +3,43 @@
 As the title says, the purpose of this repository is not to actually craft a useful Lisp variant, but to learn the basics of the C language, as well as some fundamentals on compiler design and the Lisp family of languages.
 
 The code follows the book *Build Your Own Lisp* by Daniel Holden, which is available online for free at [buildyourownlisp.com](buildyourownlisp.com). For more information, check out the first couple of chapters online.
+
+## Setup
+
+In order to be able to compile and run the project the following is needed. This project was developed on Ubuntu 21.04. For other distros look up the equivalent commands.
+
+### Tools
+
+```bash
+# Compiler (gcc) and debugger (dbg)
+sudo apt install build-essential
+```
+
+### Dependencies
+
+```bash
+# editline
+sudo apt install libedit-dev
+
+# mpc 
+# simply copy mpc.c and mpc.h from the repo onto your source folder
+git clone git@github.com:orangeduck/mpc.git
+cp mpc/{mpc.c,mpc.h} buildyourownlisp/src/
+```
+
+### Compiling, running and debugging
+
+```bash
+# compile and link against mpc, libedit and math
+gcc -std=c99 -Wall prompt.c mpc.c -ledit -lm -o prompt
+
+# run resulting executable
+./prompt
+```
+
+```bash
+# compile debug executable and start debug session with dbg
+# same as above plus -g flag
+gcc -std=c99 -Wall -g prompt.c mpc.c -ledit -lm -o prompt
+gdb prompt
+```
--- prompt.c
+++ prompt.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef _WIN32 // Windows requires no libedit, instead we provide fake substitutes
+#include "mpc.h"  // #include with "" instead of <> searches local folder first
+
+// Preprocessing directive below checks if the _WIN32 macro is defined, meaning we are on Windows
+// There exist other similar predefined macros for other OS like __linux, __APPLE__ or __ANDROID__
+#ifdef _WIN32
+
+// Windows requires no libedit since the default behaviour on the terminal gives those features
+// Instead we provide fake substitutes
 #include <string.h>
 
 static char buffer[2048];
@@ -23,25 +30,69 @@ char* readline(char* prompt) {
 
 void add_history(char* unused) {}
 
-#else // Linux & Mac require libedit for line edition and history on input
+#else
+
+// On Linux & Mac we use libedit for line edition and history on input
 #include <editline/readline.h>
 #include <editline/history.h>
+
 #endif
 
 
 int main(int argc, char** argv) {
 	
+	// Declare parsers
+	mpc_parser_t* Integer     = mpc_new("integer");
+	mpc_parser_t* Decimal     = mpc_new("decimal");
+	mpc_parser_t* Number      = mpc_new("number");
+	mpc_parser_t* OperatorSym = mpc_new("operator_sym");
+	mpc_parser_t* OperatorStr = mpc_new("operator_str");
+	mpc_parser_t* Operator    = mpc_new("operator");
+	mpc_parser_t* Expr        = mpc_new("expr");
+	mpc_parser_t* Lsp         = mpc_new("lsp");
+
+	// Define them
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"                                                                \
+		integer      : /-?[0-9]+/ ;                                      \
+		decimal      : /-?[0-9]+/ '.' /[0-9]+/ ;                         \
+		number       : <decimal> | <integer> ;                           \
+		operator_sym : '+' | '-' | '*' | '/' | '%' ;                     \
+		operator_str : \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" ; \
+		operator     : <operator_sym> | <operator_str> ;                 \
+		expr         : <number> | '(' <operator> <expr>+ ')' ;           \
+		lsp          : /^/ <operator> <expr>+ /$/ ;                      \
+		",
+		Integer, Decimal, Number, OperatorSym, OperatorStr, Operator, Expr, Lsp);
+
+	
 	// Print version and exit information
-	puts("Lsp version 0.0.0.0.1");
+	puts("Lsp version 0.0.0.0.2");
 	puts("Ctrl+C to exit\n");
 	
 	// Endlessly prompt for input and reply back
 	while (1) {
+		
 		char* input = readline("lsp> ");
 		add_history(input);
-		printf("Simon says: \"%s\"\n", input);
+		
+		// Attempt to parse user input
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lsp, &r)) {
+			// On success print AST
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			// Otherwise print error
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+		
 		free(input);
 	}
+	
+	// Undefine and delete our parsers
+	mpc_cleanup(8, Integer, Decimal, Number, OperatorSym, OperatorStr, Operator, Expr, Lsp);
 	
 	return 0;
 	
